874eb76259b8ef60fa45518b8c860d0330026133
Bo Furustam <bo.furustam@stud.pointcollege.fi>
2018-01-11 21:35:59 +0200

    Projekticontroller ilman toiminnallisuuksia

---
+++ MVCkurssitehtavaJatkokurssi/MVCkurssitehtavaJatkokurssi/Controllers/ProjektiController.cs
@@ -0,0 +1,47 @@
+﻿using MVCkurssitehtavaJatkokurssi.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCkurssitehtavaJatkokurssi.Controllers
+{
+    public class ProjektiController : Controller
+    {
+        public ActionResult Index2()
+        {
+            AsiakastietokantaEntities entities = new AsiakastietokantaEntities();
+            List<Projektit> model = entities.Projektit.ToList();
+            entities.Dispose();
+
+            return View(model);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET: Projekti
+        public JsonResult GetList()
+        {
+            AsiakastietokantaEntities entities = new AsiakastietokantaEntities();
+            //List<Projektit> model = entities.Projektit.ToList();
+
+            var model = (from p in entities.Projektit
+                         select new
+                         {
+                             ProjektiID = p.ProjektiID,
+                             Projektinimi = p.Projektinimi,
+                             
+                         }).ToList();
+
+            string json = JsonConvert.SerializeObject(model);
+            entities.Dispose();
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
